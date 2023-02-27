993dd0202310fbfef3368ae647d7dc9f17bc47d3
Bella <bella@MacBook-Pro.local>
2023-02-27 14:02:26 -0300

    site pet fofo completo

---
+++ SitePetAdocao/Produtos/JS/animals.js
@@ -0,0 +1,23 @@
+const itensJson = [{
+    id: 1,
+    name: 'Dog Lulu',
+    img: 'img/minidalmata.png',
+    price: 1.00,
+    description: 'Filhote lindo, sem raça definida. Esperando seu carinho! Idade: 6 meses'
+},
+{
+    id: 2,
+    name: 'Dog Totó',
+    img: 'img/pequenodog.png',
+    price: 1.00,
+    description: 'Cachorro pequeno, sem raça definida',
+
+},
+{
+    id: 3,
+    name: 'Cat Miau',
+    img: 'img/Gato.png',
+    price: 1.00,
+    description: 'Gatinho lindo pequeno, sem raça definida',
+}
+];
